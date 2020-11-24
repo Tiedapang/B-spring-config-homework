5e696875d4d0465032e492033d144c3eaee694ee
Tiedapang <2750428832@qq.com>
2020-11-25 00:23:08 +0800

    [thj]: finished the p2s3

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,29 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest(properties = {"levelNumber=1"})
+@AutoConfigureMockMvc
 class DemoSpringConfigApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+
+	LevelController levelController;
+
 	@Test
-	void contextLoads() {
+	public void should_get_basic_given_level_less_than_one() throws Exception {
+		mockMvc.perform(get("/level"))
+				.andExpect(MockMvcResultMatchers.content()
+						.string("advanced"));
 	}
 
 }
